1a6541485bb763ef38e54435ad435308a0c94a69
Peng-Yu Chen <me@pengyuc.com>
2024-02-17 18:06:09 -0500

    feat: add marketing page
    $ npx shadcn-ui@latest add button

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,18 @@
+import { Footer } from './_components/footer';
+import { Navbar } from './_components/navbar';
+
+interface MarketingLayoutProps {
+  children: React.ReactNode;
+}
+
+const MarketingLayout = ({ children }: MarketingLayoutProps) => {
+  return (
+    <div className="h-full bg-slate-100">
+      <Navbar />
+      <main className="bg-slate-100 pb-20 pt-40">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MarketingLayout;
